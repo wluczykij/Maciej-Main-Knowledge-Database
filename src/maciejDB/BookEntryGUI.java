ab4bfa7a1398de6cdd9d36060df4d08a1ebf33b2
wluczykij <muminek2@yahoo.com>
2016-02-19 18:33:05 -0500

    gridbag inside flow layout
    not finished, started working on gridbag inside flow layout

--- src/maciejDB/BookEntryGUI.java
+++ src/maciejDB/BookEntryGUI.java
@@ -66,7 +66,9 @@ public class BookEntryGUI extends JDialog implements BookAttributes {
 	final static Logger loggerBookEntryGUI = Logger.getLogger(BookEntryGUI.class);
 	
 	
-	private final JPanel contentPanel = new JPanel();
+	private final JPanel contentPanelFlow = new JPanel();
+	private final JPanel contentPanelGridBag1 = new JPanel();
+	private final JPanel contentPanelGridBag2 = new JPanel();
 
 	/**
 	 * Create the dialog.
@@ -80,143 +82,54 @@ public class BookEntryGUI extends JDialog implements BookAttributes {
 		
 private void initAndRunUI() {		
 		setTitle("Muminek Enterprises: BookEnryGUI");
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 606, 441);
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.NORTH);
-		GridBagLayout gbl_contentPanel = new GridBagLayout();
-		gbl_contentPanel.columnWidths = new int[]{64, 0, 61, 56, 58, 5, 101, 16, 26, 0};
-		gbl_contentPanel.rowHeights = new int[]{28, 28, 44, 0};
-		gbl_contentPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		contentPanel.setLayout(gbl_contentPanel);
-		{
-			{
-				JLabel lblFirstName = new JLabel("First Name:");
-				GridBagConstraints gbc_lblFirstName = new GridBagConstraints();
-				gbc_lblFirstName.insets = new Insets(0, 0, 5, 5);
-				gbc_lblFirstName.gridx = 1;
-				gbc_lblFirstName.gridy = 0;
-				gbc_lblFirstName.gridwidth = 1;
-				contentPanel.add(lblFirstName, gbc_lblFirstName);
-			}
+		contentPanelFlow.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanelFlow, BorderLayout.CENTER);
+		contentPanelFlow.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		{ //TODO - not sure if this parenthesis should be here!!!
+			contentPanelFlow.add(contentPanelGridBag1);
+			GridBagLayout gbl_contentPanel1 = new GridBagLayout();
+//			gbl_contentPanel1.columnWidths = new int[]{64, 0, 61, 56, 58, 5, 101, 16, 26, 0};
+//			gbl_contentPanel1.rowHeights = new int[]{28, 28, 44, 0};
+//			gbl_contentPanel1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+//			gbl_contentPanel1.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 			
-			firstNameTextField = new JTextField();			
-			firstNameTextField.getDocument().addDocumentListener(new DocumentListener() {
-				/**
-	             * If the text is changed then this event will be fired.
-	             */
-				public void changedUpdate(DocumentEvent e) {
-					loggerBookEntryGUI.debug("::firstNameTextField::changedUpdateEvent");
-					firstNameSet = true;
-					buttonsEnabler();	
+			gbl_contentPanel1.columnWidths = new int[]{64, 0, 61, 56, 58, 5, 101, 16, 26, 0};
+			gbl_contentPanel1.rowHeights = new int[]{28, 28, 0};
+			gbl_contentPanel1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+			gbl_contentPanel1.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};			
+			
+			
+			contentPanelGridBag1.setLayout(gbl_contentPanel1);
+			{
+				{
+					JLabel lblFirstName = new JLabel("First Name:");
+					GridBagConstraints gbc_lblFirstName = new GridBagConstraints();
+					gbc_lblFirstName.insets = new Insets(0, 0, 5, 5);
+					gbc_lblFirstName.gridx = 1;
+					gbc_lblFirstName.gridy = 0;
+					gbc_lblFirstName.gridwidth = 1;
+					contentPanelGridBag1.add(lblFirstName, gbc_lblFirstName);
 				}
-				/**
-	             * If some value is removed then this event is fired.
-	             */
-	            public void removeUpdate(javax.swing.event.DocumentEvent e) {
-	            	loggerBookEntryGUI.debug("::firstNameTextField::removeUpdateEvent");
-	            	firstNameSet = true;
-					buttonsEnabler();	
-	            }
-	            /**
-	             * If some value is auto set, this event will be called
-	             * @param e The value change event
-	             */
-				@Override
-				public void insertUpdate(DocumentEvent e) {
-					loggerBookEntryGUI.debug("::firstNameTextField::insertUpdateEvent");
-	            	firstNameSet = true;
-					buttonsEnabler();	
-				}	           				
-			});
-			firstNameTextField.setColumns(10);
-			GridBagConstraints gbc_firstNameTextField = new GridBagConstraints();
-			gbc_firstNameTextField.anchor = GridBagConstraints.NORTHWEST;
-			gbc_firstNameTextField.insets = new Insets(0, 0, 5, 5);
-			gbc_firstNameTextField.gridwidth = 2;
-			gbc_firstNameTextField.gridx = 2;
-			gbc_firstNameTextField.gridy = 0;
-			gbc_firstNameTextField.fill = GridBagConstraints.HORIZONTAL;
-			contentPanel.add(firstNameTextField, gbc_firstNameTextField);
-			
-			{
-				JLabel lblLastName = new JLabel("Last Name:");
-				GridBagConstraints gbc_lblLastName = new GridBagConstraints();
-				gbc_lblLastName.anchor = GridBagConstraints.EAST;
-				gbc_lblLastName.insets = new Insets(0, 0, 5, 5);
-				gbc_lblLastName.gridwidth = 1;
-				gbc_lblLastName.gridx = 4;
-				gbc_lblLastName.gridy = 0;
-				contentPanel.add(lblLastName, gbc_lblLastName);
-			}
-			
-			lastNameTextField = new JTextField();
-			lastNameTextField.getDocument().addDocumentListener(new DocumentListener() {
-				/**
-	             * If the text is changed then this event will be fired.
-	             */
-				public void changedUpdate(DocumentEvent e) {
-					loggerBookEntryGUI.debug("::lastNameTextField::changedUpdateEvent");
-					lastNameSet = true;
-					buttonsEnabler();	
-				}
-				/**
-	             * If some value is removed then this event is fired.
-	             */
-	            public void removeUpdate(javax.swing.event.DocumentEvent e) {
-	            	loggerBookEntryGUI.debug("::lastNameTextField::removeUpdateEvent");
-	            	lastNameSet = true;
-					buttonsEnabler();	
-	            }
-	            /**
-	             * If some value is auto set, this event will be called
-	             * @param e The value change event
-	             */
-				@Override
-				public void insertUpdate(DocumentEvent e) {
-					loggerBookEntryGUI.debug("::lastNameTextField::insertUpdateEvent");
-	            	lastNameSet = true;
-					buttonsEnabler();	
-				}	           				
-			});
-			lastNameTextField.setColumns(10);
-			GridBagConstraints gbc_lastNameTextField = new GridBagConstraints();
-			gbc_lastNameTextField.anchor = GridBagConstraints.NORTHWEST;
-			gbc_lastNameTextField.insets = new Insets(0, 0, 5, 5);
-			gbc_lastNameTextField.gridwidth = 2;
-			gbc_lastNameTextField.gridx = 5;
-			gbc_lastNameTextField.gridy = 0;
-			gbc_lastNameTextField.fill = GridBagConstraints.HORIZONTAL;
-			contentPanel.add(lastNameTextField, gbc_lastNameTextField);			
-			
-			{
-				JLabel lblTitle = new JLabel("Title:");
-				GridBagConstraints gbc_lblTitle = new GridBagConstraints();
-				gbc_lblTitle.anchor = GridBagConstraints.WEST;
-				gbc_lblTitle.insets = new Insets(0, 0, 5, 0);
-				gbc_lblTitle.gridx = 1;
-				gbc_lblTitle.gridy = 1;
-				contentPanel.add(lblTitle, gbc_lblTitle);
-			}
-			{
-				titleTextField = new JTextField();
-				titleTextField.getDocument().addDocumentListener(new DocumentListener() {
+				
+				firstNameTextField = new JTextField();			
+				firstNameTextField.getDocument().addDocumentListener(new DocumentListener() {
 					/**
 		             * If the text is changed then this event will be fired.
 		             */
 					public void changedUpdate(DocumentEvent e) {
-						loggerBookEntryGUI.debug("::titleTextField::changedUpdateEvent");
-						titleSet = true;
+						loggerBookEntryGUI.debug("::firstNameTextField::changedUpdateEvent");
+						firstNameSet = true;
 						buttonsEnabler();	
 					}
 					/**
 		             * If some value is removed then this event is fired.
 		             */
 		            public void removeUpdate(javax.swing.event.DocumentEvent e) {
-		            	loggerBookEntryGUI.debug("::titleTextField::removeUpdateEvent");
-		            	titleSet = true;
+		            	loggerBookEntryGUI.debug("::firstNameTextField::removeUpdateEvent");
+		            	firstNameSet = true;
 						buttonsEnabler();	
 		            }
 		            /**
@@ -225,31 +138,199 @@ private void initAndRunUI() {
 		             */
 					@Override
 					public void insertUpdate(DocumentEvent e) {
-						loggerBookEntryGUI.debug("::titleTextField::insertUpdateEvent");
-						titleSet = true;
+						loggerBookEntryGUI.debug("::firstNameTextField::insertUpdateEvent");
+		            	firstNameSet = true;
 						buttonsEnabler();	
 					}	           				
 				});
-				titleTextField.setColumns(10);
-				GridBagConstraints gbc_titleTextField = new GridBagConstraints();
-				gbc_titleTextField.anchor = GridBagConstraints.NORTHEAST;
-				gbc_titleTextField.insets = new Insets(0, 0, 5, 5);
-				gbc_titleTextField.gridwidth = 2;
-				gbc_titleTextField.gridx = 2;
-				gbc_titleTextField.gridy = 1;
-				gbc_titleTextField.fill = GridBagConstraints.HORIZONTAL;
-				contentPanel.add(titleTextField, gbc_titleTextField);
-			}			
-			
-			{
-				JLabel lblCategory = new JLabel("Category:");
-				GridBagConstraints gbc_lblCategory = new GridBagConstraints();
-				gbc_lblCategory.anchor = GridBagConstraints.WEST;
-				gbc_lblCategory.insets = new Insets(0, 0, 5, 5);
-				gbc_lblCategory.gridx = 1;
-				gbc_lblCategory.gridy = 2;
-				contentPanel.add(lblCategory, gbc_lblCategory);
+				firstNameTextField.setColumns(10);
+				GridBagConstraints gbc_firstNameTextField = new GridBagConstraints();
+				gbc_firstNameTextField.anchor = GridBagConstraints.NORTHWEST;
+				gbc_firstNameTextField.insets = new Insets(0, 0, 5, 5);
+				gbc_firstNameTextField.gridwidth = 2;
+				gbc_firstNameTextField.gridx = 2;
+				gbc_firstNameTextField.gridy = 0;
+				gbc_firstNameTextField.fill = GridBagConstraints.HORIZONTAL;
+				contentPanelGridBag1.add(firstNameTextField, gbc_firstNameTextField);				
 			}
+			contentPanelFlow.add(contentPanelGridBag2);
+			GridBagLayout gbl_contentPanel2 = new GridBagLayout();
+			gbl_contentPanel2.columnWidths = new int[]{64, 0, 61, 56, 58, 5, 101, 16, 26, 0};
+			gbl_contentPanel2.rowHeights = new int[]{28, 28, 44, 0};
+			gbl_contentPanel2.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+			gbl_contentPanel2.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+			contentPanelGridBag2.setLayout(gbl_contentPanel2);
+			{
+				{
+					JLabel lblLastName = new JLabel("Last Name:");
+					GridBagConstraints gbc_lblLastName = new GridBagConstraints();
+					gbc_lblLastName.anchor = GridBagConstraints.EAST;
+					gbc_lblLastName.insets = new Insets(0, 0, 5, 5);
+					gbc_lblLastName.gridwidth = 1;
+					gbc_lblLastName.gridx = 1;
+					gbc_lblLastName.gridy = 0;
+					contentPanelGridBag2.add(lblLastName, gbc_lblLastName);
+				}			
+			}
+			
+			
+			
+			
+			
+			
+//			{
+//				JLabel lblFirstName = new JLabel("First Name:");
+//				contentPanelFlow.add(lblFirstName);
+//			}
+//			
+//			firstNameTextField = new JTextField();			
+//			firstNameTextField.getDocument().addDocumentListener(new DocumentListener() {
+//				/**
+//	             * If the text is changed then this event will be fired.
+//	             */
+//				public void changedUpdate(DocumentEvent e) {
+//					loggerBookEntryGUI.debug("::firstNameTextField::changedUpdateEvent");
+//					firstNameSet = true;
+//					buttonsEnabler();	
+//				}
+//				/**
+//	             * If some value is removed then this event is fired.
+//	             */
+//	            public void removeUpdate(javax.swing.event.DocumentEvent e) {
+//	            	loggerBookEntryGUI.debug("::firstNameTextField::removeUpdateEvent");
+//	            	firstNameSet = true;
+//					buttonsEnabler();	
+//	            }
+//	            /**
+//	             * If some value is auto set, this event will be called
+//	             * @param e The value change event
+//	             */
+//				@Override
+//				public void insertUpdate(DocumentEvent e) {
+//					loggerBookEntryGUI.debug("::firstNameTextField::insertUpdateEvent");
+//	            	firstNameSet = true;
+//					buttonsEnabler();	
+//				}	           				
+//			});
+//			firstNameTextField.setColumns(10);
+//			GridBagConstraints gbc_firstNameTextField = new GridBagConstraints();
+//			gbc_firstNameTextField.anchor = GridBagConstraints.NORTHWEST;
+//			gbc_firstNameTextField.insets = new Insets(0, 0, 5, 5);
+//			gbc_firstNameTextField.gridwidth = 2;
+//			gbc_firstNameTextField.gridx = 2;
+//			gbc_firstNameTextField.gridy = 0;
+//			gbc_firstNameTextField.fill = GridBagConstraints.HORIZONTAL;
+//			contentPanel.add(firstNameTextField, gbc_firstNameTextField);
+//			
+//			{
+//				JLabel lblLastName = new JLabel("Last Name:");
+//				GridBagConstraints gbc_lblLastName = new GridBagConstraints();
+//				gbc_lblLastName.anchor = GridBagConstraints.EAST;
+//				gbc_lblLastName.insets = new Insets(0, 0, 5, 5);
+//				gbc_lblLastName.gridwidth = 1;
+//				gbc_lblLastName.gridx = 4;
+//				gbc_lblLastName.gridy = 0;
+//				contentPanel.add(lblLastName, gbc_lblLastName);
+//			}
+//			
+//			lastNameTextField = new JTextField();
+//			lastNameTextField.getDocument().addDocumentListener(new DocumentListener() {
+//				/**
+//	             * If the text is changed then this event will be fired.
+//	             */
+//				public void changedUpdate(DocumentEvent e) {
+//					loggerBookEntryGUI.debug("::lastNameTextField::changedUpdateEvent");
+//					lastNameSet = true;
+//					buttonsEnabler();	
+//				}
+//				/**
+//	             * If some value is removed then this event is fired.
+//	             */
+//	            public void removeUpdate(javax.swing.event.DocumentEvent e) {
+//	            	loggerBookEntryGUI.debug("::lastNameTextField::removeUpdateEvent");
+//	            	lastNameSet = true;
+//					buttonsEnabler();	
+//	            }
+//	            /**
+//	             * If some value is auto set, this event will be called
+//	             * @param e The value change event
+//	             */
+//				@Override
+//				public void insertUpdate(DocumentEvent e) {
+//					loggerBookEntryGUI.debug("::lastNameTextField::insertUpdateEvent");
+//	            	lastNameSet = true;
+//					buttonsEnabler();	
+//				}	           				
+//			});
+//			lastNameTextField.setColumns(10);
+//			GridBagConstraints gbc_lastNameTextField = new GridBagConstraints();
+//			gbc_lastNameTextField.anchor = GridBagConstraints.NORTHWEST;
+//			gbc_lastNameTextField.insets = new Insets(0, 0, 5, 5);
+//			gbc_lastNameTextField.gridwidth = 2;
+//			gbc_lastNameTextField.gridx = 5;
+//			gbc_lastNameTextField.gridy = 0;
+//			gbc_lastNameTextField.fill = GridBagConstraints.HORIZONTAL;
+//			contentPanel.add(lastNameTextField, gbc_lastNameTextField);			
+//			
+//			{
+//				JLabel lblTitle = new JLabel("Title:");
+//				GridBagConstraints gbc_lblTitle = new GridBagConstraints();
+//				gbc_lblTitle.anchor = GridBagConstraints.WEST;
+//				gbc_lblTitle.insets = new Insets(0, 0, 5, 0);
+//				gbc_lblTitle.gridx = 1;
+//				gbc_lblTitle.gridy = 1;
+//				contentPanel.add(lblTitle, gbc_lblTitle);
+//			}
+//			{
+//				titleTextField = new JTextField();
+//				titleTextField.getDocument().addDocumentListener(new DocumentListener() {
+//					/**
+//		             * If the text is changed then this event will be fired.
+//		             */
+//					public void changedUpdate(DocumentEvent e) {
+//						loggerBookEntryGUI.debug("::titleTextField::changedUpdateEvent");
+//						titleSet = true;
+//						buttonsEnabler();	
+//					}
+//					/**
+//		             * If some value is removed then this event is fired.
+//		             */
+//		            public void removeUpdate(javax.swing.event.DocumentEvent e) {
+//		            	loggerBookEntryGUI.debug("::titleTextField::removeUpdateEvent");
+//		            	titleSet = true;
+//						buttonsEnabler();	
+//		            }
+//		            /**
+//		             * If some value is auto set, this event will be called
+//		             * @param e The value change event
+//		             */
+//					@Override
+//					public void insertUpdate(DocumentEvent e) {
+//						loggerBookEntryGUI.debug("::titleTextField::insertUpdateEvent");
+//						titleSet = true;
+//						buttonsEnabler();	
+//					}	           				
+//				});
+//				titleTextField.setColumns(10);
+//				GridBagConstraints gbc_titleTextField = new GridBagConstraints();
+//				gbc_titleTextField.anchor = GridBagConstraints.NORTHEAST;
+//				gbc_titleTextField.insets = new Insets(0, 0, 5, 5);
+//				gbc_titleTextField.gridwidth = 2;
+//				gbc_titleTextField.gridx = 2;
+//				gbc_titleTextField.gridy = 1;
+//				gbc_titleTextField.fill = GridBagConstraints.HORIZONTAL;
+//				contentPanel.add(titleTextField, gbc_titleTextField);
+//			}			
+//			
+//			{
+//				JLabel lblCategory = new JLabel("Category:");
+//				GridBagConstraints gbc_lblCategory = new GridBagConstraints();
+//				gbc_lblCategory.anchor = GridBagConstraints.WEST;
+//				gbc_lblCategory.insets = new Insets(0, 0, 5, 5);
+//				gbc_lblCategory.gridx = 1;
+//				gbc_lblCategory.gridy = 2;
+//				contentPanel.add(lblCategory, gbc_lblCategory);
+//			}
 //			{
 //				categoryComboBox = new JComboBox();
 //				// TODO need to set it to default - not set
